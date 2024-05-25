6d549bf41c38be61842eec66d6fb0e030cf5e94d
Paramveer Singh <paramsinghkhattra@gmail.com>
2024-05-25 12:15:44 +0530

    Spring Web Starter and Spring JPA code

--- src/main/java/com/example/learning/Application.java
+++ src/main/java/com/example/learning/Application.java
@@ -2,8 +2,12 @@ package com.example.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.learning")
+@EntityScan("com.example.learning.entity")
+@EnableJpaRepositories(basePackages = "com.example.learning.repository")
 public class Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/learning/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.learning.repository;
+
+import com.example.learning.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
