a06a0c1e2371301090dad4781213943899dcfcb8
Paramveer Singh <paramsinghkhattra@gmail.com>
2024-05-26 21:48:58 +0530

    Spring Actuator

---
+++ src/main/java/com/example/learning/CustomHealthIndicator.java
@@ -0,0 +1,24 @@
+package com.example.learning;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomHealthIndicator implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        int errorCode = check(); // perform some specific health check
+        if (errorCode != 0) {
+            return Health.down().withDetail("Error Code", errorCode).build();
+        }
+        return Health.up().build();
+    }
+
+    private int check() {
+        // Your logic to check health
+        return 0; // return non-zero for an error condition
+    }
+}
+
--- src/main/java/com/example/learning/config/SecurityConfig.java
+++ src/main/java/com/example/learning/config/SecurityConfig.java
@@ -11,19 +11,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeRequests(authorizeRequests ->
-                        authorizeRequests
-                                .antMatchers("/", "/home").permitAll()  // Allow everyone to see the home page
-                                .anyRequest().authenticated()  // Require authentication for any other request
-                )
-                .formLogin(formLogin ->
-                        formLogin
-                                .loginPage("/login")  // Specify the custom login page URL
-                                .permitAll()  // Allow all users to view the login page
-                )
-                .logout(logout ->
-                        logout.permitAll()  // Allow all users to logout
-                )
+                .authorizeRequests(authorizeRequests -> {
+                    authorizeRequests
+                            .antMatchers("/", "/home").permitAll()  // Allow everyone to see the home page
+                            .anyRequest().authenticated();  // Require authentication for any other request
+                })
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")  // Specify the custom login page URL
+                        .permitAll())  // Allow all users to view the login page
+                .logout(logout -> logout.permitAll())  // Allow all users to logout
                 .build();  // Build the SecurityFilterChain instance
     }
 }
